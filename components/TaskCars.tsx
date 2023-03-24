36fc524e33b59dbb9aa3a43eda30fba368bde46f
ProSweeper <dylan.doucet28@gmail.com>
2023-03-24 03:30:22 +0000

    start task card

---
+++ components/TaskCars.tsx
@@ -0,0 +1,25 @@
+import { getUserFromCookie } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { TASK_STATUS } from "@prisma/client";
+import { cookies } from "next/headers";
+import Button from "./Button";
+import Card from "./Card";
+
+const getData = async () => {
+  const user = await getUserFromCookie(cookies());
+  const tasks = await db.task.findMany({
+    where: {
+      ownerId: user?.id,
+      NOT: {
+        status: TASK_STATUS.COMPLETED,
+        deleted: false,
+      },
+    },
+    take: 5,
+    orderBy: {
+      due: "asc",
+    },
+  });
+
+  return tasks;
+}
